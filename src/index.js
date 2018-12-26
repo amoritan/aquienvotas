3a5f8fb2f773be7f19f466587b5be772ec4eaee1
Andres Moritan <amoritan@me.com>
2018-12-26 09:30:53 -0300

    Axios installed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
