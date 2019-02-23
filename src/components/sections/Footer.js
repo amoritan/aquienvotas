3942c6416a09efa608ac3847ecc1c68b7733489e
Andres Moritan <amoritan@me.com>
2019-02-23 20:40:20 -0300

    Minor footer fixes

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -4,8 +4,6 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faTwitter, faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import SectionTitle from '../styled/SectionTitle'
-
 library.add(faTwitter, faFacebook, faInstagram)
 
 const Container = styled.footer`
@@ -24,9 +22,9 @@ function Footer() {
   return (
     <Container>
       <a href="mailto:hola@aquienvotas.com">hola@aquienvotas.com</a><br />
-      <a href="https://twitter.com/aquienvotas" target="_blank" title="#AQuienVotas en Twitter"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
-      <a href="https://www.instagram.com/aquienvotasok/" target="_blank" title="#AQuienVotas en Instagram"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
-      <a href="https://www.facebook.com/aquienvotascom" target="_blank" title="#AQuienVotas en Facebook"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
+      <a href="https://twitter.com/aquienvotas" target="_blank" rel="noopener noreferrer" title="#AQuienVotas en Twitter"><FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+      <a href="https://www.instagram.com/aquienvotasok/" target="_blank" rel="noopener noreferrer" title="#AQuienVotas en Instagram"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
+      <a href="https://www.facebook.com/aquienvotascom" target="_blank" rel="noopener noreferrer" title="#AQuienVotas en Facebook"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
     </Container>
   )
 }
