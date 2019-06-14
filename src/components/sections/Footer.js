4943a62dca398f60e2725e36f2f0ee9dc4a18b15
Andres Moritan <amoritan@me.com>
2019-06-14 00:14:38 -0300

    Creative Commons license added

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -53,6 +53,10 @@ const Container = styled.footer`
       margin: 0;
       text-decoration: underline;
     }
+    img {
+      background: #fff;
+      margin-right: 1em;
+    }
   }
 `
 
@@ -65,10 +69,19 @@ function Footer() {
       <a href="https://www.facebook.com/aquienvotasok" target="_blank" rel="noopener noreferrer" title="#AQuienVotas en Facebook"><FontAwesomeIcon icon={['fab', 'facebook']} /></a>
       <a href="https://github.com/andresmoritan/aquienvotas" target="_blank" rel="noopener noreferrer" title="Repositorio de #AQuienVotas en GitHub"><FontAwesomeIcon icon={['fab', 'github']} /></a>
       <aside>
-        <p>AQuienVotas. Copyright (C) 2015-2019 Andres Moritan.</p>
-        <p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.</p>
-        <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.</p>
-        <p>You should have received a copy of the GNU Affero General Public License along with this program. If not, see <a href="https://www.gnu.org/licenses/" target="_blank" rel="noopener noreferrer">https://www.gnu.org/licenses/</a>.</p>
+        
+        <p><a xmlnsCc="http://creativecommons.org/ns#" href="https://www.aquienvotas.com" property="cc:attributionName" rel="cc:attributionURL">AQuienVotas</a> <span xmlnsDct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Dataset" property="dct:title" rel="dct:type">Results</span> are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</p>
+        <br />
+
+        <p>AQuienVotas Program Copyright (C) 2015-2019 Andres Moritan. This program is free software: you can redistribute it and/or modify it under the terms of the <a href="https://www.gnu.org/licenses/agpl-3.0.html" rel="license" >GNU Affero General Public License</a> as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.</p>
+        <br />
+
+        <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">
+          <img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" />
+        </a>
+        <a rel="license" href="https://www.gnu.org/licenses/agpl-3.0.html">
+          <img alt="GNU Affero General Public License" src="https://www.gnu.org/graphics/agplv3-88x31.png" />
+        </a>
       </aside>
     </Container>
   )
