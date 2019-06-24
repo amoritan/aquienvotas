8f21619ea1c498d8786ca71f22d3f535bbb29fdc
Andres Moritan <amoritan@me.com>
2019-06-24 16:03:35 -0300

    Companion support added to Candidate component

--- src/components/elements/Candidate.js
+++ src/components/elements/Candidate.js
@@ -24,7 +24,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { darken } from 'polished'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -37,21 +37,20 @@ const Container = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  background-image: ${ props => `linear-gradient(${ props.color } 0%, ${ props.color } 55%, #fefefe 55%, #fefefe 100%)` };
+  background-image: ${ props => `linear-gradient(${ props.color } 0%, ${ props.color } 60%, #fefefe 60%, #fefefe 100%)` };
   border-radius: .5em;
   max-width: 12em;
   padding: 1em .5em;
-  height: 18em;
+  height: 20em;
   width: calc(50% - 1rem);
   box-sizing: border-box;
   margin: .5rem;
   @media (min-width: 48rem) {
     width: calc(25% - 1rem);
   }
-  h3, h4 {
+  h3, h4, h5 {
     margin: 0 0 .5rem 0;
     height: 2em;
-    line-height: 1em;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -60,8 +59,9 @@ const Container = styled.article`
   h3 {
     font-size: 1em;
     font-weight: 600;
-    vertical-align: middle;
     color: #fefefe;
+    align-items: flex-end;
+    margin-bottom: 0;
   }
   h4 {
     font-size: .8em;
@@ -69,15 +69,38 @@ const Container = styled.article`
     color: ${ props => darken(0.25, props.color) };
     text-transform: uppercase;
   }
+  h5 {
+    font-size: .8em;
+    font-weight: 600;
+    color: #fefefe;
+    box-shadow: 0 -.0625rem 0 0 #fefefe;
+    align-items: flex-start;
+    padding-top: .15rem;
+  }
+  div {
+    flex-shrink: 2;
+    position: relative;
+  }
   img {
     display: block;
     margin: 0 auto;
-    width: 80%;
+    width: 60%;
     height: auto;
     border-radius: 100%;
-    flex-shrink: 2;
-    border: .25em solid ${ props => props.color };
+    border: .20em solid ${ props => props.color };
     background: ${ props => props.color };
+    ${props => props.hasCompanion && css`
+      position: relative;
+      z-index: 2;
+      left: -17.5%;
+      &.companion {
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        left: 42.5%;
+        transform: scale(.8);
+      }
+    `};
   }
   button {
     margin-top: 1em;
@@ -106,10 +129,14 @@ function Candidate(props) {
   }
 
   return (
-    <Container color={ `#${ props.data.color || props.data.party.color }` }>
+    <Container color={ `#${ props.data.color || props.data.party.color }` } hasCompanion={ Boolean(props.data.companion) }>
       <h3>{ props.data.name }</h3>
+      { props.data.companion ? <h5>{ props.data.companion.name }</h5> : '' }
       <h4>{ props.data.party.name }</h4>
-      <img src={ props.data.avatar || '/images/avatar.png' } alt={ props.data.name } width="240" height="240" />
+      <div>
+        <img src={ props.data.avatar || '/images/avatar.png' } alt={ props.data.name } width="240" height="240" />
+        { props.data.companion ? <img src={ props.data.companion.avatar || '/images/avatar.png' } alt={ props.data.companion.name } width="240" height="240" class="companion" /> : '' }
+      </div>
       <button onClick={ handleVote }><FontAwesomeIcon icon="vote-yea" /> Votar</button>
     </Container>
   )
@@ -129,7 +156,12 @@ Candidate.propTypes = {
       description: PropTypes.string,
       color: PropTypes.string.isRequired,
       result: PropTypes.number
-    }).isRequired
+    }).isRequired,
+    companion: PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string
+    })
   }).isRequired,
   voteHandler: PropTypes.func.isRequired
 }
